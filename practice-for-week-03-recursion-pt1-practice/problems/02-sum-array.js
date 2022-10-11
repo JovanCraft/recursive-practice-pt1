5c934a02f78f025fd6a3f905a552840e88fa44b7
Jhass Theisen <jhasstheisen1@gmail.com>
2022-10-11 10:18:04 -0700

    Finished problem 2

--- practice-for-week-03-recursion-pt1-practice/problems/02-sum-array.js
+++ practice-for-week-03-recursion-pt1-practice/problems/02-sum-array.js
@@ -9,12 +9,28 @@ sumArray([1, 2, 3]); //  6
 sumArray([0, 1, -3]); //  -2
 sumArray([1, 2, 3, 4, 5]); // 15
 
+// takes in array of ints
+// go through each index,
+// add current and next index,
+// call to do same
 //
+
+// return arr with first i + second i
+// remove first index
+// when only 1 num in array, that's result
 
 ***********************************************************************/
 
 function sumArray(arr) {
   // Your code here
+  let newArr = arr;
+  if (arr.length === 1) {
+    return arr[0];
+  }
+  let firstNum = newArr.shift();
+  newArr[0] = newArr[0] + firstNum;
+  return sumArray(newArr);
+
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
