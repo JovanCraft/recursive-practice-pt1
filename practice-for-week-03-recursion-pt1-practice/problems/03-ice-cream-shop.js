896769ab8747c464299a44a4da93bf7467744f2d
jovan <jovancraft@gmail.com>
2022-10-11 13:04:55 -0500

    finished 03

--- practice-for-week-03-recursion-pt1-practice/problems/03-ice-cream-shop.js
+++ practice-for-week-03-recursion-pt1-practice/problems/03-ice-cream-shop.js
@@ -10,14 +10,38 @@ iceCreamShop(['pistachio', 'green tea', 'chocolate', 'mint chip'], 'green tea');
 iceCreamShop(['cookies n cream', 'blue moon', 'superman', 'honey lavender', 'sea salt caramel'], 'pistachio'); // false
 iceCreamShop(['moose tracks'], 'moose tracks'); // true
 iceCreamShop([], 'honey lavender'); // false
+
+
+/*
+1. takes in flavors array, and string favorite
+2. check if index 0 is favorite, if yes return true
+3. if no take off first num
+4. call func without first num , putting it fav
+5. if array.length 0, return false
+
 ***********************************************************************/
 
 
 function iceCreamShop(flavors, favorite) {
   // Your code here
+
+  if (flavors[0] === favorite){
+    return true
+  }
+
+  if (flavors.length === 0){
+    return false
+  }
+  
+  flavors.shift()
+  return iceCreamShop(flavors, favorite)
 }
 
-
+iceCreamShop(['vanilla', 'strawberry'], 'blue moon'); // false
+iceCreamShop(['pistachio', 'green tea', 'chocolate', 'mint chip'], 'green tea'); // true
+iceCreamShop(['cookies n cream', 'blue moon', 'superman', 'honey lavender', 'sea salt caramel'], 'pistachio'); // false
+iceCreamShop(['moose tracks'], 'moose tracks'); // true
+iceCreamShop([], 'honey lavender'); // false
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = iceCreamShop;
